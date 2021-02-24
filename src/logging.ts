868edb14d47bea57f595fd218d4a047f55a16580
Sam Vervaeck <samvv@pm.me>
2021-02-24 15:21:23 +0100

    Improve log output a bit

--- src/logging.ts
+++ src/logging.ts
@@ -1,17 +1,18 @@
+import chalk from "chalk";
 
 const isDebug = process.env['NODE_ENV'] === 'development';
 
 export function error(message: string): void {
-  console.error(`Error: ${message}`);
+  console.error(chalk.bold.red(` error `) + message);
 }
 
 export function verbose(message: string): void {
   if (isDebug) {
-    console.info(message);
+    console.info(chalk.bold.magenta(' verb ') + message);
   }
 }
 
 export function info(message: string): void {
-  console.info(message);
+  console.info(chalk.bold.yellow(' info ') + message);
 }
 
